5e1a9e2b93096da5bcbf3674021b53d60472b5b2
Chopardissmo <gel@alumni.usc.edu>
2021-04-27 02:05:45 +0800

    upadte method name

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -18,7 +18,7 @@ export default class Header extends Component {
         }
         const newTodo = {
             id: nanoid(),
-            name: target.value,
+            name: target.value.trim(),
             done: false,
         };
         // pass the new todoObj from item(child) to App(parent) component
